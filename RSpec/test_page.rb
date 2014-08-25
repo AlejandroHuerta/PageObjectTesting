0643f289d7f03ec3d75b2de1b211e108903ebeed
AlejandroHuerta <alejandro.huerta@mckesson.com>
2014-08-25 15:05:00 -0500

    set checkbox with correct selector

--- RSpec/test_page.rb
+++ RSpec/test_page.rb
@@ -12,6 +12,6 @@ class TestPage
     create_list 'List1', selector(:ul, id: 'list'), click_object('Result', selector(:a, {}))
     create_list 'List2', selector(:ul, id: 'compound_list'), [click_object('Link', selector(:a, {})), text_field('TextField', selector(:text_field, {}))]
 
-    create_checkbox 'I have a bike', selector(:input, value: 'Bike')
+    create_checkbox 'I have a bike', selector(:checkbox, value: 'Bike')
   end
 end
