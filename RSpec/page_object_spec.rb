41041beba70bfb0d6b25d6d7aabbeb00d9bd34e8
Alejandro Huerta <jano.a.huerta@gmail.com>
2014-09-29 22:06:08 -0500

    reorganized test

--- RSpec/page_object_spec.rb
+++ RSpec/page_object_spec.rb
@@ -16,71 +16,9 @@ describe Page do
     @browser.send :close
   end
 
-  describe PageObject::ElementObject do
-
-    before(:each) do
-      @browser.send :goto, "file://#{Dir.pwd}\\TestPage.html"
-      Page.page = TestPage.new @browser
-    end#do
-
-    it '.send(:see)' do
-      expect(Page.page.send('Dud').send(:see)).to equal(true)
-    end#do
-
-    describe PageObject::ClickElement do
-
-      it '.send(:click)' do
-        expect(Page.page.send('Dud').send(:click)).to be_instance_of(Array)
-      end#do
-
-      it '.send(:click) driver added method is called' do
-        expect(Page.page.send('Mouse Down').send(:click)).to equal(true)
-      end
-    end#ClickElement
-
-    describe PageObject::TextInputElement do
-
-      it ".send(:set, 'text')" do
-        expect(Page.page.send('First name').send(:set, 'Test')).to equal(nil)
-      end#do
-
-      it '.send(:value)' do
-        Page.page.send('First name').send(:set, 'Test')
-
-        expect(Page.page.send('First name').send(:value)).to eq('Test')
-      end#do
-    end#TextInputElement
-
-    describe PageObject::ListElement do
-
-      it '.send(#).send(:click)' do
-        expect(Page.page.send('List1').send(1).send(:click)).to be_instance_of(Array)
-      end#do
-
-      it ".send(#).send('name').send(:set, 'text')" do
-        expect(Page.page.send('List2').send(0).send('TextField').send(:set, 'Test')).to equal(nil)
-      end#do
-    end#ListElement
-
-    describe PageObject::CheckboxElement do
-
-      it ".send('name').send(:check)" do
-        expect(Page.page.send('I have a bike').send(:check)).to equal(nil)
-      end#do
-    end#CheckboxElement
-
-    describe PageObject::SaveElement do
-
-      it '.send(:save)' do
-        expect(Page.page.send('Reddit').send(:save)).to eq('Reddit')
-      end#do
-
-      it '.send(:value)' do
-        Page.page.send('Reddit').send(:save)
-        expect(Page.page.send('Reddit').send(:value)).to eq('Reddit')
-      end#do
-    end#SaveElement
-  end#ElementObject
+  it '.send(NAME)' do
+    expect(Page.page.send('W3Schools')).to be_instance_of(PageObject::ClickElement)
+  end
 
   it 'changes pages' do
     @browser.send :goto, "file://#{Dir.pwd}\\TestPage.html"
@@ -90,3 +28,129 @@ describe Page do
   end#do
 
 end#Page
+
+describe PageObject::ElementObject do
+
+  before(:all) do
+    @driver = WatirDriver.new
+    Page.page = TestPage.new @driver
+  end
+
+  after(:all) do
+    @driver.send :close
+  end
+
+  before(:each) do
+    @driver.send :goto, "file://#{Dir.pwd}\\TestPage.html"
+  end#do
+
+  it '.send(:see)' do
+    e = PageObject::ElementObject.new @driver,
+                                      'name',
+                                      selector: PageObject::Selector.new(:button, id: 'dud_button')
+
+    expect(e.send(:see)).to equal(true)
+  end#do
+
+  describe PageObject::ClickElement do
+
+    it '.send(:click)' do
+      e = PageObject::ClickElement.new @driver,
+                                       'name',
+                                       selector: PageObject::Selector.new(:button, id: 'dud_button')
+
+      expect(e.send(:click)).to be_instance_of(Array)
+    end#do
+
+    it '.send(:click) driver added method is called' do
+      e = PageObject::ClickElement.new @driver,
+                                       'name',
+                                       selector: PageObject::Selector.new(:button, id: 'dud_button'),
+                                       actions: {click: :on_mouse_down}
+
+      expect(e.send(:click)).to equal(true)
+    end
+  end#ClickElement
+
+  describe PageObject::TextInputElement do
+
+    it '.send(:set, TEXT)' do
+      e = PageObject::TextInputElement.new @driver,
+                                           'First name',
+                                           selector: PageObject::Selector.new(:text_field, id: 'tf1')
+
+
+      expect(e.send(:set, 'Test')).to equal(nil)
+    end#do
+
+    it '.send(:value)' do
+      e = PageObject::TextInputElement.new @driver,
+                                           'First name',
+                                           selector: PageObject::Selector.new(:text_field, id: 'tf1')
+
+      e.send(:set, 'Test')
+      expect(e.send(:value)).to eq('Test')
+    end#do
+  end#TextInputElement
+
+  describe PageObject::ListElement do
+
+    it '.send(#).send(:click)' do
+      e = PageObject::ListElement.new @driver,
+                                      'List1',
+                                      selector: PageObject::Selector.new(:ul, id: 'list'),
+                                      children: PageObject::ClickElement.new(@driver,
+                                                                             'Result',
+                                                                             selector: PageObject::Selector.new(:a))
+
+      expect(e.send(1).send(:click)).to be_instance_of(Array)
+    end#do
+
+    it '.send(#).send(NAME).send(:set, TEXT)' do
+      e = PageObject::ListElement.new @driver,
+                                      'List2',
+                                      selector: PageObject::Selector.new(:ul, id: 'compound_list'),
+                                      children: [PageObject::ClickElement.new(@driver,
+                                                                              'Link',
+                                                                              selector: PageObject::Selector.new(:a)),
+                                                 PageObject::TextInputElement.new(@driver,
+                                                                                  'TextField',
+                                                                                  selector: PageObject::Selector.new(:text_field))]
+
+      expect(e.send(0).send('TextField').send(:set, 'Test')).to equal(nil)
+    end#do
+  end#ListElement
+
+  describe PageObject::CheckboxElement do
+
+    it '.send(NAME).send(:check)' do
+      e =  PageObject::CheckboxElement.new @driver,
+                                           'I have a bike',
+                                           selector: PageObject::Selector.new(:checkbox, value: 'Bike')
+
+      expect(e.send(:check)).to equal(nil)
+    end#do
+  end#CheckboxElement
+
+  describe PageObject::SaveElement do
+
+    it '.send(:save)' do
+      e = PageObject::SaveElement.new @driver,
+                                      'Reddit',
+                                      selector: PageObject::Selector.new(:a, id: 'link3'),
+                                      actions: {save: :text}
+
+      expect(e.send(:save)).to eq('Reddit')
+    end#do
+
+    it '.send(:value)' do
+      e = PageObject::SaveElement.new @driver,
+                                      'Reddit',
+                                      selector: PageObject::Selector.new(:a, id: 'link3'),
+                                      actions: {save: :text}
+
+      e.send(:save)
+      expect(e.send(:value)).to eq('Reddit')
+    end#do
+  end#SaveElement
+end#ElementObject
