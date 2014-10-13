0b19d6f5363d08e91f1acd9bc1802c2c68763af1
AlejandroHuerta <alejandro.huerta@mckesson.com>
2014-10-13 15:00:56 -0500

    Pass a block to make button presses that don't have the same button disappear not time out

--- Driver/rautomation_driver.rb
+++ Driver/rautomation_driver.rb
@@ -14,4 +14,11 @@ end
 module PageObject
   #Action filters for this driver
   ElementObject.actions_dictionary = {see: :exists?}
+
+  require_relative '../Elements/click_element'
+  class ClickElement
+    def click(driver, *args)
+      driver.send(@params[:actions][:click], *args){true}
+    end
+  end
 end #PageObject
