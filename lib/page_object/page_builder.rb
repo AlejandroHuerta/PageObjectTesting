258727397d6508dd0cbaefc4dc12bdab9803ab28
AlejandroHuerta <alejandro.huerta@mckesson.com>
2014-10-27 11:43:02 -0500

    Added remaining objects and enabled childing

--- lib/page_object/page_builder.rb
+++ lib/page_object/page_builder.rb
@@ -3,26 +3,54 @@ require_relative 'page'
 module PageObject
   module PageBuilder
     class << self
-      attr_reader :page, :element
+      attr_reader :stack
+
       def define(&block)
-        @page = Page.new @driver
+        @stack ||= []
+        @stack.push Page.new
         instance_eval &block if block_given?
-        @page
-      end
+        @stack.pop
+      end#define
 
       def element(name, &block)
-        @element = ElementObject.new name: name
-        instance_eval &block if block_given?
-        @page.children[name] = @element
-      end
+        create_element ElementObject, name, &block
+      end#element
 
       def selector(hash)
-        @element.params[:selector][0] = Selector.new hash
-      end
+        @stack.last.params[:selector].push Selector.new hash
+      end#selector
 
       def actions(hash)
-        @element.params[:actions].merge!(hash) {|_key, _oldval, newval| newval}
-      end
-    end
-  end
-end
+        @stack.last.params[:actions].merge!(hash) {|_key, _oldval, newval| newval}
+      end#actions
+
+      def click(name, &block)
+        create_element ClickElement, name, &block
+      end#click
+
+      def checkbox(name, &block)
+        create_element CheckboxElement, name, &block
+      end#checkbox
+
+      def list(name, &block)
+        create_element ListElement, name, &block
+      end#list
+
+      def save(name, &block)
+        create_element SaveElement, name, &block
+      end#save
+
+      def text_input(name, &block)
+        create_element TextInputElement, name, &block
+      end#text_input
+
+      private
+      def create_element(klass, name, &block)
+        @stack.push Object::const_get(klass.name).new name: name
+        instance_eval &block if block_given?
+        this = @stack.pop
+        @stack.last.params[:children][name] = this
+      end#create_element
+    end#class << self
+  end#PageBuilder
+end#PageObject
--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -8,48 +8,98 @@ context 'Page' do
     test_page = PageBuilder.define
 
     expect(test_page).to be_instance_of(Page)
-  end
-end
+  end#it
+end#context Page
 
-context 'Element' do
+context 'ElementObject' do
   it 'creates an element' do
     test_page = PageBuilder.define do
       element 'Element'
-    end
+    end#define
 
     expect(test_page.send('Element')).to be_instance_of(ElementObject)
-  end
+  end#it
 
   it 'element with selector' do
     test_page = PageBuilder.define do
       element 'Element' do
         selector type: :button, specifier: {id: 'form2button'}
-      end
-    end
+      end#element
+    end#define
 
     expect(test_page.send('Element').params[:selector][0]).to_not eql(nil)
     expect(test_page.send('Element').params[:selector][0].type).to equal(:button)
     expect(test_page.send('Element').params[:selector][0].specifier).to eql({id: 'form2button'})
-  end
+  end#it
 
   it 'element with actions' do
     test_page = PageBuilder.define do
       element 'Element' do
         actions text: :text, see: :with_my_eyes
-      end
-    end
+      end#element
+    end#define
 
     expect(test_page.send('Element').params[:actions].has_key? :text).to equal(true)
     expect(test_page.send('Element').params[:actions][:see]).to equal(:with_my_eyes)
-  end
+  end#it
 
   it 'element with child' do
     test_page = PageBuilder.define do
       element 'Element' do
         element 'Child'
-      end
-    end
+      end#element
+    end#define
 
     expect(test_page.send('Element').send('Child')).to be_instance_of(ElementObject)
-  end
-end
+  end#it
+end#context Element
+
+context 'ClickElement' do
+  it 'creates a click element' do
+    test_page = PageBuilder.define do
+      click 'ClickElement'
+    end#define
+
+    expect(test_page.send('ClickElement')).to be_instance_of(ClickElement)
+  end#it
+end#context ClickElement
+
+context 'CheckboxElement' do
+  it 'creates a checkbox element' do
+    test_page = PageBuilder.define do
+      checkbox 'CheckboxElement'
+    end#define
+
+    expect(test_page.send('CheckboxElement')).to be_instance_of(CheckboxElement)
+  end#it
+end#context CheckboxElement
+
+context 'ListElement' do
+  it 'creates a list element' do
+    test_page = PageBuilder.define do
+      list 'ListElement'
+    end#define
+
+    expect(test_page.send('ListElement')).to be_instance_of(ListElement)
+  end#it
+end#context ListElement
+
+context 'SaveElement' do
+  it 'creates a save element' do
+    test_page = PageBuilder.define do
+      save 'SaveElement'
+    end#define
+
+    expect(test_page.send('SaveElement')).to be_instance_of(SaveElement)
+  end#it
+end#context SaveElement
+
+context 'TextInputElement' do
+  it 'creates a text input element' do
+    test_page = PageBuilder.define do
+      text_input 'TextInputElement'
+    end#define
+
+    expect(test_page.send('TextInputElement')).to be_instance_of(TextInputElement)
+  end#it
+end#context TextInputElement
