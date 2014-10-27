1379c55e8937736179bbe9c83195989145184e59
AlejandroHuerta <alejandro.huerta@mckesson.com>
2014-10-27 10:19:07 -0500

    Page now holds a params hash much like ElementObject

--- lib/page_object/page_object.rb
+++ lib/page_object/page_object.rb
@@ -8,34 +8,34 @@ require_relative '../elements/save_element'
 
 module PageObject
 
-  def create_element(_hash)
-    create_element_object :element, _hash
+  def create_element(hash)
+    create_element_object :element, hash
   end
 
-  def create_click_object(_hash)
-    create_element_object :click_object,  _hash
+  def create_click_object(hash)
+    create_element_object :click_object,  hash
   end
 
-  def create_text_field( _hash)
-    create_element_object :text_field, _hash
+  def create_text_field( hash)
+    create_element_object :text_field, hash
   end
 
-  def create_list(_hash)
-    create_element_object :list, _hash
+  def create_list(hash)
+    create_element_object :list, hash
   end
 
-  def create_checkbox(_hash)
-    create_element_object :checkbox_object, _hash
+  def create_checkbox(hash)
+    create_element_object :checkbox_object, hash
   end
 
-  def create_save(_hash)
-    create_element_object :save_element, _hash
+  def create_save(hash)
+    create_element_object :save_element, hash
   end
 
   protected
-  def create_element_object(_element, _hash)
-    raise ArgumentError, 'Argument not a hash' unless _hash.instance_of? Hash
-    @children[_hash[:name]] = self.send(_element, _hash)
+  def create_element_object(element, hash)
+    raise ArgumentError, 'Argument not a hash' unless hash.instance_of? Hash
+    @params[:children][hash[:name]] = self.send(element, hash)
   end
 end #module page_object
 
